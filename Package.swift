6cad85caa9f302e1967e21ea980d6f7289c7516e
RobLabs.com <roblabs@users.noreply.github.com>
2023-10-18 14:35:34 -0700

    Use DocC to build and share documentation for non-swift projects — https://github.com/roblabs/swift-docc-template

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "XYZ Raster Sources",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0")
+    ]
+)
+
